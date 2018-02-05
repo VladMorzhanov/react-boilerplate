eef99ab02a42b82ca5e8c01b862ceb4e9dc1117f
VladMorzhanov <vlad.morzhanov@gmail.com>
2018-02-05 21:04:46 +0200

    new server

--- server/index.js
+++ server/index.js
@@ -1,26 +1,34 @@
 /* eslint consistent-return:0 */
 
+const {API_URI} = require('./constants')
 const express = require('express')
 const logger = require('./logger')
-
+const cors = require('cors')
+const {json, urlencoded} = require('body-parser')
 const argv = require('./argv')
+const signals = ['SIGINT', 'SIGTERM']
 const port = require('./port')
+const {api} = require('./routers')
 const setup = require('./middlewares/frontendMiddleware')
 const isDev = process.env.NODE_ENV !== 'production'
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false
 const resolve = require('path').resolve
 const app = express()
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+app.use(cors())
+app.use(json())
+app.use(urlencoded({extended: false}))
+app.use(API_URI, api)
 
-// In production we need to pass these values in instead of relying on webpack
+// setup database
+// const db = require('./db')
+// app.set('db', db)
+
 setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
   publicPath: '/'
 })
 
-// get the intended host and port number, use localhost and port 3000 if not provided
 const customHost = argv.host || process.env.HOST
 const host = customHost || null // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost'
